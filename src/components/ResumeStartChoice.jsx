9c382dcd214003eb24c8e7d82e7961319676ce37
Ayo <Ayoroq@gmail.com>
2025-11-01 10:27:48 -0400

    feat: implement resume choice component with new layout and icons

--- src/components/ResumeStartChoice.jsx
+++ src/components/ResumeStartChoice.jsx
@@ -1,12 +1,15 @@
+import add from '../assets/add.svg'
+import upload from '../assets/upload.svg'
+
 export default function ResumeChoice({onSelectChoice}){
     return(
-        <div className="resume-choice">
+        <div className="resume-choice-container">
             <div className="new-data choice" onClick={() => onSelectChoice('new')}>
-                <p></p>
+                <img src={add} alt="Image of a plus sign" />
                 <p>New Blank Resume</p>
             </div>
             <div className="import-data choice" onClick={() => onSelectChoice('import')}>
-                <p></p>
+                <img src={upload} alt="Image of a file upload sign" />
                 <p>Import Resume</p>
             </div>
         </div>
