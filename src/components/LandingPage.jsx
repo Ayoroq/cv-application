39f4c75366e6f34d8d2f96715f1152181d0b12d9
Ayo <Ayoroq@gmail.com>
2025-11-08 10:22:23 -0500

    Added more information to the landing page HTML.

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,24 @@
+export default function LandingPage() {
+  return (
+    <main className="main">
+      <div className="main-primary">
+        <h1 className="main-primary-text">Build your résumé in minutes!</h1>
+        <p>
+          Create, edit, and download beautiful resumes right from your browser.
+        </p>
+        <button className="start">Start building now - It's free!</button>
+      </div>
+      <div className="main-secondary">
+        <h3 className="main-secondary-text">Simple. Fast. Customizable</h3>
+        <p>
+          Vitae is a simple, fast, and customizable résumé builder that allows
+          you to create a beautiful résumé in minutes. Free and open-source,
+          it's built to help anyone design a résumé that's uniquely theirs. With
+          a wide array of templates and flexible editing options, Vitae makes
+          the process of building your résumé effortless, enjoyable, and
+          efficient.
+        </p>
+      </div>
+    </main>
+  );
+}
